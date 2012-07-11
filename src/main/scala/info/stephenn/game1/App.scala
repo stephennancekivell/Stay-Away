b01850377357bb1693865ed0b6326c4c43d18fee
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-07-11 23:41:43 +1200

    use slick. hello world.

--- src/main/scala/info/stephenn/game1/App.scala
+++ src/main/scala/info/stephenn/game1/App.scala
@@ -1,8 +1,11 @@
 package info.stephenn.game1
 
-/**
- * @author ${user.name}
- */
+import org.newdawn.slick.AppGameContainer
+import org.newdawn.slick.BasicGame
+import org.newdawn.slick.GameContainer
+import org.newdawn.slick.Graphics
+import org.newdawn.slick.SlickException
+
 object App {
   
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
@@ -10,6 +13,24 @@ object App {
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
+    
+    val app = new AppGameContainer(new App())
+ 
+         app.setDisplayMode(800, 600, false);
+         app.start();
+    
+    
   }
 
+}
+
+class App extends BasicGame("Slick2DPath2Glory - SimpleGame") {
+ 
+  
+  override def init(gc: GameContainer):Unit = {}
+  
+  override def update(gc: GameContainer, delta: Int): Unit = {}
+  
+  override def render(gc: GameContainer, g: Graphics): Unit = {}
+  
 }
