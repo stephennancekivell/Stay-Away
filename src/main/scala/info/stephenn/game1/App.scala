6a049128e0a4d4f5ab87ab25d25b32040a9b1bb5
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-07-14 19:07:04 +1200

    use swing instead of slick. Because slick, lwjgl needs annoying native libraries

--- src/main/scala/info/stephenn/game1/App.scala
+++ src/main/scala/info/stephenn/game1/App.scala
@@ -1,36 +1,29 @@
 package info.stephenn.game1
 
-import org.newdawn.slick.AppGameContainer
-import org.newdawn.slick.BasicGame
-import org.newdawn.slick.GameContainer
-import org.newdawn.slick.Graphics
-import org.newdawn.slick.SlickException
+import java.awt._
+import javax.swing._
 
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-    
-    val app = new AppGameContainer(new App())
- 
-         app.setDisplayMode(800, 600, false);
-         app.start();
-    
-    
+  def main(args: Array[String]) {
+    new App().go
   }
-
 }
 
-class App extends BasicGame("Slick2DPath2Glory - SimpleGame") {
- 
+class App extends Canvas {
+  val frame = new JFrame();
+
+  def go = {
+    frame.setSize(400, 400)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    frame.getContentPane.add(this)
+    frame.setVisible(true)
+  }
   
-  override def init(gc: GameContainer):Unit = {}
+  def draw:Unit = {
+    
+  }
   
-  override def update(gc: GameContainer, delta: Int): Unit = {}
-  
-  override def render(gc: GameContainer, g: Graphics): Unit = {}
-  
+  def tick:Unit = {
+    
+  }
 }
