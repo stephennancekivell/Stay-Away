f6b2657f7008a06958c9c077f9a03b5065fd1ffe
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2012-07-14 20:13:55 +1200

    logging

--- src/main/scala/info/stephenn/game1/App.scala
+++ src/main/scala/info/stephenn/game1/App.scala
@@ -13,20 +13,28 @@ object App {
 class App extends Canvas {
   val log = LogFactory.getLog(getClass)
   val frame = new JFrame()
+  var x1 = 10
 
   override def paint(graphics: Graphics) = {
-    graphics.drawRect(10, 20, 10, 20)
+    graphics.drawRect(x1, 20, 10, 20)
   }
 
   def go = {
+    log.info("go")
     frame.setSize(400, 400)
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     frame.getContentPane.add(this)
     frame.setVisible(true)
+    
+    while(true){
+      Thread.sleep(100)
+      updateLogic
+    }
   }
 
-  def tick: Unit = {
-    log.trace("tick")
+  def updateLogic: Unit = {
+    log.info("tick")
+    x1 += 1
 
   }
 }
